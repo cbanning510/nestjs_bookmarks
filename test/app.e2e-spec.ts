04194dbd532b7d1df34d89110e459cec673c1808
Jack Banning <jack1119banning@gmail.com>
2023-03-01 14:07:41 -0500

    8 total tests passing for signup and signin 3:00:32

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,6 +34,28 @@ describe('App e2e', () => {
       password: '123',
     };
     describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('should throw if no body', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
       it('should signup', () => {
         return pactum
           .spec()
@@ -44,6 +66,27 @@ describe('App e2e', () => {
     });
 
     describe('Signin', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('should throw if no body', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
       it('should signin', () => {
         return pactum
           .spec()
